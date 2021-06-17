4dc75ecd7f8797d3a713feb740872f4ef4dc1519
Kajetan Rzepecki <k@spartan.works>
2021-06-18 00:58:55 +0200

    Moved the macros & tests to separate files.

--- synclo.scm
+++ synclo.scm
@@ -410,192 +410,20 @@
 
 ;; Example:
 
-(pretty-print
- (expand scheme-environment
-         '(begin
-            ;; Some of these macros were taken from the great Chibi Scheme implementation.
-            ;; Copyright (c) 2009-2012 Alex Shinn.  All rights reserved.
-            ;; BSD-style license: http://synthcode.com/license.txt
-            (define-syntax cond
-              (er-macro-transformer
-               (lambda (expr rename compare)
-                 (if (null? (cdr expr))
-                     (if #f #f)
-                     ((lambda (cl)
-                        (if (compare (rename 'else) (car cl))
-                            (if (pair? (cddr expr))
-                                (error "non-final else in cond" expr)
-                                (cons (rename 'begin) (cdr cl)))
-                            (if (if (null? (cdr cl)) #t (compare (rename '=>) (cadr cl)))
-                                (list (list (rename 'lambda) (list (rename 'tmp))
-                                            (list (rename 'if) (rename 'tmp)
-                                                  (if (null? (cdr cl))
-                                                      (rename 'tmp)
-                                                      (list (car (cddr cl)) (rename 'tmp)))
-                                                  (cons (rename 'cond) (cddr expr))))
-                                      (car cl))
-                                (list (rename 'if)
-                                      (car cl)
-                                      (cons (rename 'begin) (cdr cl))
-                                      (cons (rename 'cond) (cddr expr))))))
-                      (cadr expr))))))
-            (define-syntax and
-              (er-macro-transformer
-               (lambda (expr rename compare)
-                 (cond ((null? (cdr expr)))
-                       ((null? (cddr expr)) (cadr expr))
-                       (else (list (rename 'if) (cadr expr)
-                                   (cons (rename 'and) (cddr expr))
-                                   #f))))))
-            (define-syntax quasiquote
-              (er-macro-transformer
-               (lambda (expr rename compare)
-                 (define qq
-                   (lambda (x d)
-                     (cond
-                      ((pair? x)
-                       (cond
-                        ((compare (rename 'unquote) (car x))
-                         (if (<= d 0)
-                             (cadr x)
-                             (list (rename 'list) (list (rename 'quote) 'unquote)
-                                   (qq (cadr x) (- d 1)))))
-                        ((compare (rename 'unquote-splicing) (car x))
-                         (if (<= d 0)
-                             (list (rename 'cons) (qq (car x) d) (qq (cdr x) d))
-                             (list (rename 'list) (list (rename 'quote) 'unquote-splicing)
-                                   (qq (cadr x) (- d 1)))))
-                        ((compare (rename 'quasiquote) (car x))
-                         (list (rename 'list) (list (rename 'quote) 'quasiquote)
-                               (qq (cadr x) (+ d 1))))
-                        ((and (<= d 0) (pair? (car x))
-                              (compare (rename 'unquote-splicing) (caar x)))
-                         (if (null? (cdr x))
-                             (cadr (car x))
-                             (list (rename 'append) (cadr (car x)) (qq (cdr x) d))))
-                        (else
-                         (list (rename 'cons) (qq (car x) d) (qq (cdr x) d)))))
-                      ((vector? x) (list (rename 'list->vector) (qq (vector->list x) d)))
-                      ((if (identifier? x) #t (null? x)) (list (rename 'quote) x))
-                      (else x))))
-                 (qq (cadr expr) 0))))
-            (define-syntax case
-              (er-macro-transformer
-               (lambda (expr rename compare)
-                 (define body
-                   (lambda (exprs)
-                     (cond
-                      ((null? exprs)
-                       (rename 'tmp))
-                      ((compare (rename '=>) (car exprs))
-                       `(,(cadr exprs) ,(rename 'tmp)))
-                      (else
-                       `(,(rename 'begin) ,@exprs)))))
-                 (define clause
-                   (lambda (ls)
-                     (cond
-                      ((null? ls) #f)
-                      ((compare (rename 'else) (caar ls))
-                       (body (cdar ls)))
-                      ((and (pair? (car (car ls))) (null? (cdr (car (car ls)))))
-                       `(,(rename 'if) (,(rename 'eqv?) ,(rename 'tmp)
-                                        (,(rename 'quote) ,(car (caar ls))))
-                         ,(body (cdar ls))
-                         ,(clause (cdr ls))))
-                      (else
-                       `(,(rename 'if) (,(rename 'memv) ,(rename 'tmp)
-                                        (,(rename 'quote) ,(caar ls)))
-                         ,(body (cdar ls))
-                         ,(clause (cdr ls)))))))
-                 `(let ((,(rename 'tmp) ,(cadr expr)))
-                    ,(clause (cddr expr))))))
-            (define-syntax let
-              (er-macro-transformer
-               (lambda (expr rename compare)
-                 (if (null? (cdr expr)) (error "empty let" expr))
-                 (if (null? (cddr expr)) (error "no let body" expr))
-                 ((lambda (bindings)
-                    (if (list? bindings)
-                        #f
-                        (error "bad let bindings"))
-                    (if (every (lambda (x)
-                                 (if (pair? x)
-                                     (if (pair? (cdr x))
-                                         (null? (cddr x))
-                                         #f)
-                                     #f))
-                               bindings)
-                        ((lambda (vars vals)
-                           (if (identifier? (cadr expr))
-                               `((,(rename 'lambda) ,vars
-                                  (,(rename 'letrec) ((,(cadr expr)
-                                                       (,(rename 'lambda) ,vars
-                                                        ,@(cdr (cddr expr)))))
-                                   (,(cadr expr) ,@vars)))
-                                 ,@vals)
-                               `((,(rename 'lambda) ,vars ,@(cddr expr)) ,@vals)))
-                         (map car bindings)
-                         (map cadr bindings))
-                        (error "bad let syntax" expr)))
-                  (if (identifier? (cadr expr)) (car (cddr expr)) (cadr expr))))))
-            (define-syntax or
-              (er-macro-transformer
-               (lambda (expr rename compare)
-                 (cond ((null? (cdr expr)) #f)
-                       ((null? (cddr expr)) (cadr expr))
-                       (else
-                        (list (rename 'let) (list (list (rename 'tmp) (cadr expr)))
-                              (list (rename 'if) (rename 'tmp)
-                                    (rename 'tmp)
-                                    (cons (rename 'or) (cddr expr)))))))))
-            ;; Some tests for the macros.
-            (cond ((or (something? x)
-                       (something-else? y))
-                   do this stuff)
-                  ((otherwise?)
-                   (let ((x 23))
-                     (* x x)))
-                  (else
-                   23))
-            (case 23
-              ((23) #t)
-              ((5 7 13) 'nope)
-              (else
-               ':shrug:))
-            (lambda (let*)
-              (let ((a b))
-                c))
-            (lambda (let)
-              (let ((a b))
-                c))
-            (lambda (let)
-              (let* ((a b))
-                c))
-            (display `(list ,(+ 2 2) "wut"))
-            (define-syntax let
-              (sc-macro-transformer
-               (lambda (exp def-env)
-                 `(begin
-                    (display "letting ")
-                    (display ',(cadr exp))
-                    (display " to do ")
-                    (display ',(caddr exp))
-                    (newline)))))
-            (lambda (let)
-              (let ((a b))
-                c))
-            (lambda (let*)
-              (let ((a b))
-                c))
-            (define-syntax foo
-              (sc-macro-transformer
-               (lambda (exp def-env)
-                 (define foo
-                   (lambda (a b)
-                     (+ a b)))
-                 `(+ ,(cadr exp)
-                     ,(caddr exp)
-                     ,(foo (cadr exp)
-                           (caddr exp))))))
-            (foo 5 23))
-         resulting-value))
+(let ((code (with-input-from-file
+                "tests.scm"
+              (lambda ()
+                (read)))))
+  (expand scheme-environment
+          (with-input-from-file
+              "init.scm"
+            (lambda ()
+              (read)))
+          (lambda (env _)
+            (expand env
+                    code
+                    (lambda (_ expanded)
+                      (displayln "Input code:")
+                      (pretty-print code)
+                      (displayln "Expanded code:")
+                      (pretty-print expanded))))))
---
+++ tests.scm
@@ -0,0 +1,61 @@
+;; Some tests for the macros.
+(begin
+  (cond ((or (something? x)
+             (something-else? y))
+         do this stuff)
+        ((otherwise?)
+         (let ((x 23))
+           (* x x)))
+        (else
+         23))
+
+  (case 23
+    ((23) #t)
+    ((5 7 13) 'nope)
+    (else
+     ':shrug:))
+
+  (lambda (let*)
+    (let ((a b))
+      c))
+
+  (lambda (let)
+    (let ((a b))
+      c))
+
+  (lambda (let)
+    (let* ((a b))
+      c))
+
+  (display `(list ,(+ 2 2) "wut"))
+
+  (define-syntax let
+    (sc-macro-transformer
+     (lambda (exp def-env)
+       `(begin
+          (display "letting ")
+          (display ',(cadr exp))
+          (display " to do ")
+          (display ',(caddr exp))
+          (newline)))))
+
+  (lambda (let)
+    (let ((a b))
+      c))
+
+  (lambda (let*)
+    (let ((a b))
+      c))
+
+  (define-syntax foo
+    (sc-macro-transformer
+     (lambda (exp def-env)
+       (define foo
+         (lambda (a b)
+           (+ a b)))
+       `(+ ,(cadr exp)
+           ,(caddr exp)
+           ,(foo (cadr exp)
+                 (caddr exp))))))
+
+  (foo 5 23))
